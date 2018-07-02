72e44bb4310df7cd6214d49e7fe8ea5c1bb6b210
mehrbarz <mehrbarzegar@gmail.com>
2018-07-02 13:31:45 +0200

    added 10 longest words in the dictionary finder in linux

--- src/JAVAIO.java
+++ src/JAVAIO.java
@@ -1,9 +1,23 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.stream.Stream;
 
 public class JAVAIO {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		try (Stream<String> lines = Files.lines(Paths.get("/usr/share/dict/words"))){
+			lines.filter(s -> s.length() > 20)
+			.sorted(Comparator.comparingInt(String::length).reversed())
+			.limit(10)
+			.forEach(w -> System.out.printf("%s (%d) %n" , w, w.length()));
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
 	}
 
 }
