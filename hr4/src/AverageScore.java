94fd7e01b4598ec6f068f589015f3c4cdbc824d0
mehrbarz <mehrbarzegar@gmail.com>
2020-11-11 07:42:03 +0100

    average

--- hr4/src/AverageScore.java
+++ hr4/src/AverageScore.java
@@ -1,10 +1,49 @@
 package src;
 
+import java.util.Scanner;
+
+/**
+ * if statement
+ * @author MehrBarz
+ *
+ */
+
 public class AverageScore {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double score1, 
+		       score2,
+		       score3,
+		       average;
+		
+		// Create a Scanner object to read input. 
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("This program averages 3 test scores. ");
+		
+		// Get the first score.
+		System.out.println("Enter score #1: ");
+		score1 = keyboard.nextDouble();
+		
+		// Get the second score.
+		System.out.println("Enter score #2: ");
+		score2 = keyboard.nextDouble();
+		
+		// Get the third score. 
+		System.out.println("Enter score #3: ");
+		score3 = keyboard.nextDouble();
+		
+		// Calculate and display the average score. 
+		average = (score1 + score2 + score3)/ 3.0;
+		System.out.println("The average is " + average);
+		
+		//If the average is higher than 90, congratulate the user.
+		if(average > 90)
+			System.out.println("You made a great score! ");
+		
 
+				
 	}
 
 }
